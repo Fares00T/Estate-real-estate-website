cc4b799489be7c94e428703214b3f4988ba64e8a
Fares00T <fa.tinakiche@gmail.com>
2025-03-12 13:54:09 +0100

    vercalurl

--- api/app.js
+++ api/app.js
@@ -5,17 +5,27 @@ import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.rout.js";
 import cookieParser from "cookie-parser";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Directly set your frontend URL
+    origin: process.env.CLIENT_URL, // Using environment variable
     credentials: true,
   })
 );
 
+/*
+app.use(
+  cors({
+    //origin: "http://localhost:5173", // Directly set your frontend URL
+    origin: process.env.CLIENT_URL, // Directly set your frontend URL
+    credentials: true,
+  })
+);
+*/
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
 app.use("/api/users", userRoute);
