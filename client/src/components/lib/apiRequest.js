12d939bc53a629c7dbb10f027f36acdfb6e555f4
Fares00T <fa.tinakiche@gmail.com>
2025-03-12 14:10:49 +0100

    api changes

--- client/src/components/lib/apiRequest.js
+++ client/src/components/lib/apiRequest.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const apiRequest = axios.create({
   //baseURL: "http://localhost:8800/api",
-  baseURL: "https://pfe-56gpback.vercel.app/",
+  //baseURL: "https://pfe-56gpback.vercel.app/",
+  baseURL: `${import.meta.env.VITE_BACK_END_URL}`,
   withCredentials: true,
 });
 
