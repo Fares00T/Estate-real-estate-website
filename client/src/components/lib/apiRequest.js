fb94f01a70ec7af7312f1fa825fbbc9558b85b59
Fares00T <62139647+Fares00T@users.noreply.github.com>
2025-03-12 02:40:26 +0100

    Update apiRequest.js

--- client/src/components/lib/apiRequest.js
+++ client/src/components/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  //baseURL: "http://localhost:8800/api",
+  baseURL: "https://pfe-vnum.onrender.com"
   withCredentials: true,
 });
 
